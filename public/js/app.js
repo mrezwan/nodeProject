83f581c4296915be5860fee91ce15871f263f454
mrezwan <r.rajib0@gmail.com>
2025-01-22 22:44:07 +0600

    location error modification

--- public/js/app.js
+++ public/js/app.js
@@ -17,16 +17,14 @@ webForm.addEventListener('submit', (e) => {
 
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
